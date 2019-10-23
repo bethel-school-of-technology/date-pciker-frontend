279dd0c95824a13ff080535ccd4131996101f1d4
Andrew Searcy <andrewsearcy@Andrews-Mac-Pro.local>
2019-10-23 12:21:42 -0500

    Added Login Form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React from "react";
+import FamilyDateNight from "../logos/Family Date Night.png";
+import { Link } from "react-router-dom";
 
-const Login = () => (
-  <div>
-  </div>
-)
-
+const Login = () => {
+  return (
+    <form>
+      <div className="form-logo">
+        <img
+          src={FamilyDateNight}
+          alt="Family Logo"
+        />
+      </div>
+      <div className="form-login">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button className='login-button'>Login</button>
+      </div>
+      <p className="signup-text">
+        You can also click here to {" "}
+        <Link style={{ textDecoration: "none" }} to="/signup">
+          Sign-up
+        </Link>
+      </p>
+    </form>
+  );
+};
 export default Login;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 
 const Signup = () => (
-  <div>
-    <form></form>
-  </div>
+    <form>
+      
+    </form>
 )
 
 export default Signup;
